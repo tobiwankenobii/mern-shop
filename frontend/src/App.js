6e7670e8bc0e9616266ec9d9cbae2a0703912a1f
tobiwankenobii <tobiv1337@gmail.com>
2020-12-24 14:25:37 +0100

    Initialize react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <h1>Welcome to the MERN Shop!</h1>
+            <p>Built</p>
+        </>
+    );
+};
+
+export default App;
