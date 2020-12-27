461e0f7f47c48134b7c7afc3e01a9ba28144b4f9
tobiwankenobii <tobiv1337@gmail.com>
2020-12-27 17:30:44 +0100

    Add middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 import { config } from 'dotenv';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 config();
 
@@ -9,14 +10,11 @@ connectDB();
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
