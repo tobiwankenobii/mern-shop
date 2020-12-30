0fbf9cfb0f5ba0e093700faa42a9039599ea3905
tobiwankenobii <tobiv1337@gmail.com>
2020-12-30 21:24:14 +0100

    Add quantity select and add to cart button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import {
+    Row,
+    Col,
+    Image,
+    ListGroup,
+    Card,
+    Button,
+    Form,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,7 +15,8 @@ import { fetchProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+    const [qty, setQty] = useState(0);
     const dispatch = useDispatch();
 
     const { loading, error, product } = useSelector(
@@ -17,6 +26,18 @@ const ProductScreen = ({ match }) => {
     useEffect(() => {
         dispatch(fetchProductDetails(match.params.id));
     }, [dispatch, match]);
+
+    const qtyOptions = new Array(product.countInStock)
+        .fill(0)
+        .map((_, index) => (
+            <option key={index + 1} value={index + 1}>
+                {index + 1}
+            </option>
+        ));
+
+    const addToCart = () => {
+        history.push(`/cart/${match.params.id}?qty${qty}`);
+    };
 
     return (
         <>
@@ -76,11 +97,35 @@ const ProductScreen = ({ match }) => {
                                         </Col>
                                     </Row>
                                 </ListGroup.Item>
+
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty</Col>
+                                            <Col>
+                                                <Form.Control
+                                                    as="select"
+                                                    value={qty}
+                                                    onChange={(e) =>
+                                                        setQty(e.target.value)
+                                                    }
+                                                >
+                                                    {qtyOptions}
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                )}
+
                                 <ListGroup.Item>
                                     <Button
+                                        onClick={addToCart}
                                         className="btn-block"
                                         type="button"
-                                        disabled={product.countInStock === 0}
+                                        disabled={
+                                            product.countInStock === 0 ||
+                                            loading
+                                        }
                                     >
                                         Add to cart
                                     </Button>
