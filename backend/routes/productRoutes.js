8ddd627d9fab27ffca11466a691ba65f8e02c6ee
tobiwankenobii <tobiv1337@gmail.com>
2021-01-19 21:07:27 +0100

    Fix product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,12 +6,13 @@ import {
     getTopProducts,
     deleteProduct,
     updateProduct,
+    createProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
 router.get('/top', getTopProducts);
 router
